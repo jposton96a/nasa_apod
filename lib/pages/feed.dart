99765490c5f02ff2abf9cbb789af4448238cf3ad
Jacob Poston <dev@jacobposton.com>
2021-03-27 15:18:07 -0400

    Experiment with second page

---
+++ lib/pages/feed.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class APODFeedPage extends StatefulWidget {
+  APODFeedPage({Key key}) : super(key: key);
+
+  final title = "Astronomy Picture of the Day";
+
+  @override
+  _APODFeedPageState createState() => _APODFeedPageState();
+}
+
+class _APODFeedPageState extends State<APODFeedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Container(
+              height: 50,
+              color: Colors.amber[600],
+              child: const Center(child: Text('Entry A')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[500],
+              child: const Center(child: Text('Entry B')),
+            ),
+            Container(
+              height: 50,
+              color: Colors.amber[100],
+              child: const Center(child: Text('Entry C')),
+            ),
+          ],
+        ));
+  }
+}
